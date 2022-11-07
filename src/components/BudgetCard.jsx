ca133ccca2ab3d66d24bfbdae9d5caae68e84813
Alexandre Camilo <alex.camilo6@hotmail.ch>
2022-11-07 13:48:10 +0100

    total budget card done

--- src/components/BudgetCard.jsx
+++ src/components/BudgetCard.jsx
@@ -1,7 +1,7 @@
 import { Card, ProgressBar, Stack, Button } from "react-bootstrap";
 import { currencyFormatter } from "../utils.js";
 
-export default function BudgetCard({ name, amount, max, gray, onAddExpenseClick }) {
+export default function BudgetCard({ name, amount, max, gray, onAddExpenseClick, hiddenButton }) {
   const classNames = []
     if(amount > max) {
       classNames.push('bg-danger', 'bg-opacity-10')
@@ -28,10 +28,10 @@ export default function BudgetCard({ name, amount, max, gray, onAddExpenseClick 
           max={max}
           now={amount}
         /> }
-        <Stack direction="horizontal" gap="2" className="mt-5">
+        {!hiddenButton && <Stack direction="horizontal" gap="2" className="mt-5">
           <Button variant="outline-primary" className="ms-auto" onClick={onAddExpenseClick}>Add Expense</Button>
           <Button variant="outline-secondary">View Expenses</Button>
-        </Stack>
+        </Stack>}
       </Card.Body>
     </Card>
   );
